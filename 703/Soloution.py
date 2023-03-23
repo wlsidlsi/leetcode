8f76472255e55f9d28c92a86aa921b83c3a76346
Christopher Kane <chris.ao2i0@aleeas.com>
2023-03-22 19:33:05 -0500

    leetcode problems

---
+++ 703/Soloution.py
@@ -0,0 +1,40 @@
+# Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+# 
+#  
+# 
+# Example 1:
+# 
+# Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+# Output: 6
+# Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+# Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+# 
+# Example 2:
+# 
+# Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+# Output: 10
+# Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+# Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+# 
+#  
+# 
+# Constraints:
+# 
+#     1 <= nums.length <= 105
+#     nums[i] is either 0 or 1.
+#     0 <= k <= nums.length
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+        for right in range(len(nums)):
+            # If we included a zero in the window we reduce the value of k.
+            # Since k is the maximum zeros allowed in a window.
+            k -= 1 - nums[right]
+            # A negative k denotes we have consumed all allowed flips and window has
+            # more than allowed zeros, thus increment left pointer by 1 to keep the window size same.
+            if k < 0:
+                # If the left element to be thrown out is zero we increase k.
+                k += 1 - nums[left]
+                left += 1
+        return right - left + 1
